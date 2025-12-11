249c8c5cb36f567649e8adc28312117677d9446c
rayrayrayrayray5 <raymondl187@nycstudents.net>
2025-12-11 13:59:40 -0500

    all i see is magic aiden liu

---
+++ app/src/main.js
@@ -0,0 +1,17 @@
+async function getData(poke) {
+  try {
+    //get data from API
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${poke}`);
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      //converts response into json we can use
+      const data = await response.json();
+      document.getElementById("api-response").textContent = data.name;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getData("Squirtle");
