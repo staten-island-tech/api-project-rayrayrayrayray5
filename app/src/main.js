ef8bf751ff7ffd8d8bd80c29766442b05cb973f6
rayrayrayrayray5 <raymondl187@nycstudents.net>
2026-01-06 14:00:32 -0500

    2012 aiden liu prime

--- app/src/main.js
+++ app/src/main.js
@@ -1,19 +1,32 @@
-async function getData(number) {
+async function getData(type, number) {
   try {
     //get data from API
     const response = await fetch(
-      `https://rickandmortyapi.com/api/character/${number}`
+      `https://rickandmortyapi.com/api/${type}/${number}`
     );
     if (response.status != 200) {
       throw new Error(response);
     } else {
       //converts response into json we can use
       const data = await response.json();
-      console.log(data);
+      document.getElementById("api-response").textContent = data.content;
     }
   } catch (error) {
     console.log(error);
   }
 }
 
-getData("36");
+getData("location", "36");
+
+function inject(item) {
+  const container = document.querySelector(".app");
+  container.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="card" genre-id="${item.genre}" song-id="${item.title}">
+        <img class="png" src="${item.cover}"> 
+        <h2>${item.title}</h2>
+        <h2 class="artist">${item.artist}</h2>
+        <h3 class="length">${item.length}</h3>
+      </div>`
+  );
+}
