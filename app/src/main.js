d6a7fb73e35aaaecb61f83d82361a5025c70efdf
rayrayrayrayray5 <raymondl187@nycstudents.net>
2026-01-11 00:42:57 -0500

    anna lausanity

--- app/src/main.js
+++ app/src/main.js
@@ -1,26 +1,33 @@
 async function getData() {
   function inject(item) {
-    const container = document.querySelector(".app");
+    const container = document.querySelector(".flex");
     container.insertAdjacentHTML(
-      "afterbegin",
-      `   <img class="png" src="${item.image}"> 
-        <h2>${item.name}</h2>
-        <h2 class="artist">${item.status}</h2>
-        <h2 class="length">${item.species}</h2>
+      "beforeend",
+      `<div class="bg-white rounded-xl overflow-hidden">
+      <img class="w-full h-75 object-cover" src="${item.image}"> 
+      <div class="text-base p-4 space-y-1">
+        <h2 class="font-semibold">${item.name} </h2>
+        <h3 class=>${item.status} </h3>
+        <h4>${item.species}</h4>
+        </div>
       </div>`
     );
   }
   try {
     //get data from API
-    const response = await fetch(`https://rickandmortyapi.com/api/character`);
-    if (response.status != 200) {
-      throw new Error(response);
-    } else {
-      //converts response into json we can use
-      const data = await response.json();
-      data.results.forEach((item) => {
-        inject(item);
-      });
+    for (let page = 1; page <= 10; page++) {
+      const response = await fetch(
+        `https://rickandmortyapi.com/api/character?page=${page}`
+      );
+      if (response.status != 200) {
+        throw new Error(response);
+      } else {
+        //converts response into json we can use
+        const data = await response.json();
+        data.results.forEach((item) => {
+          inject(item);
+        });
+      }
     }
   } catch (error) {
     console.log(error);
