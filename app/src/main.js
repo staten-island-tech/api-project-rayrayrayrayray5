e565da312fbd0608509b2cbbce61f609811b17d3
rayrayrayrayray5 <raymondl187@nycstudents.net>
2026-01-14 13:27:19 -0500

    yo lausanity

--- app/src/main.js
+++ app/src/main.js
@@ -55,7 +55,7 @@ async function search(id) {
   } catch (error) {
     clear();
     document.querySelector("section").innerHTML =
-      "<h1> class='text-3xl'>Available IDs are 1-826</h1>";
+      "<h1> class=text-3xl>Available IDs are 1-826</h1>";
   }
 }
 
